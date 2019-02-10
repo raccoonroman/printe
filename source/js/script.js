fcf30af840b785caf55f424df4899efde7d190e8
Роман <kromanjs@gmail.com>
2019-02-10 17:55:33 +0200

    add header-top

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,21 @@
 "use strict";
 
 $('.search-button').click(function(){
-  $(this).parent().toggleClass('open');
-   $(this).prev().focus();
+	$(this).parent().toggleClass('open');
+	$(this).prev().focus();
+});
+
+
+var toggle = document.querySelector(".header-top__toggle");
+var navMain = document.querySelector('.header-nav__list');
+
+
+toggle.addEventListener('click', function() {
+	if (toggle.classList.contains('header-top__toggle--active')) {
+		toggle.classList.remove('header-top__toggle--active');
+		navMain.classList.remove('header-nav__list--active');
+	} else {
+		toggle.classList.add('header-top__toggle--active');
+		navMain.classList.add('header-nav__list--active');
+	}
 });
